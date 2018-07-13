2e1cbcbb7faa7f580bee8e4a383c7f5a31715f06
Afras Ali <afras.ali21@gmail.com>
2018-07-14 00:11:00 +0530

    Add files via upload
    InitialCommit 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
+///<reference path="../../node_modules/primeng/components/inputtext/inputtext.d.ts"/>
 import { BrowserModule } from '@angular/platform-browser';
+import {MenubarModule} from 'primeng/menubar';
+
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import { LoginComponent } from './login/login.component';
+import {ButtonModule} from 'primeng/button';
+import {MenuItem} from 'primeng/api';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AutoCompleteModule,
+    FormsModule,
+    ButtonModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
